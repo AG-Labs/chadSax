f1d2d64ccb357eb45264a718079095337d6d99fe
Andrew Godley <agdevlabs@gmail.com>
2019-12-08 16:35:03 +0000

    untested mode select

--- chadSax.ino
+++ chadSax.ino
@@ -1,18 +1,3 @@
-/*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fade
-*/
-
 int ledR = 9;           // the PWM pin the LED is attached to
 int ledB = 6;           // the PWM pin the LED is attached to
 int ledG = 11;           // the PWM pin the LED is attached to
@@ -21,28 +6,57 @@ int fadeAmount = 5;    // how many points to fade the LED by
 int bOffset = 0;
 int gOffset = 0;
 
+int modePin = 123;   ////////////// 
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
   pinMode(ledR, OUTPUT);
   pinMode(ledB, OUTPUT);
   pinMode(ledG, OUTPUT);
+  pinMode(modePin, INPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
-  analogWrite(ledR, brightness);
-  //analogWrite(ledB, (brightness + bOffset));
+  int mode = 0;
+  int reading = digitalRead(modePin);
 
-
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
   }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+      mode++;
+        
+      }
+    }
+  }
+  lastButtonState = reading;
+  
+  switch (mode){
+    case 0:
+      Serial.print("mode 1");
+      break;
+    case 1:
+      Serial.print("mode 2");
+      break;
+    case 2:
+      Serial.print("mode 3");
+      break;
+    case 3:
+      Serial.print("mode 4");
+      break;
+    default:
+      Serial.print("mode default");
+      break;
+  }
 }
